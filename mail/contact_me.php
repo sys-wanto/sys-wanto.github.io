7955c5dc4b0726d7b323af39c644dbef3b5f53c8
refanvolis <seallover321@gmail.com>
2018-09-03 15:04:46 +0700

    change content

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -7,9 +7,9 @@ if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || 
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
+/*
 // Create the email and send the message
 $to = "yourname@yourdomain.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
@@ -17,6 +17,11 @@ $body = "You have received a new message from your website contact form.\n\n"."H
 $header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
+*/
+$send = "Hai, my name : ".$name."\n";
+$send .="From : ".$email."\n";
+$send .="Message : ".$message."\n\n";
+$send_message = urlencode($send);
+header("Location: https://wa.me/628813317486?text=".$send_message);
+die();
 ?>
